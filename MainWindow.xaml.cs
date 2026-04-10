044a0522dd4c85c31f675769bfe928a6ed5dfd68
Timom <timerlan.nur1990@gmail.com>
2026-04-10 22:19:48 +0500

    Initial commit of WindowCompanyApp

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System.Windows;
+
+namespace WindowCompanyApp
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // Загружаем страницу с клиентами по умолчанию
+            MainFrame.Navigate(new ClientsPage());
+            StatusText.Text = "Загружена страница клиентов";
+        }
+
+        private void btnClients_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ClientsPage());
+            StatusText.Text = "📋 Показаны клиенты";
+        }
+
+        private void btnOrders_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new OrdersPage());
+            StatusText.Text = "📦 Показаны заказы";
+        }
+
+        private void btnProducts_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new ProductsPage());
+            StatusText.Text = "🪟 Показана продукция";
+        }
+
+        private void btnEmployees_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Navigate(new EmployeesPage());
+            StatusText.Text = "👥 Показаны сотрудники";
+        }
+    }
+}
---
+++ OrdersPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System.Linq;
+using System.Windows.Controls;
+
+namespace WindowCompanyApp
+{
+    public partial class OrdersPage : Page
+    {
+        private Галерея_оконEntities db = new Галерея_оконEntities();
+
+        public OrdersPage()
+        {
+            InitializeComponent();
+            LoadData();
+        }
+
+        private void LoadData()
+        {
+            var orders = db.orders
+                .Select(o => new
+                {
+                    o.id,
+                    Клиент = o.clients.full_name,
+                    Менеджер = o.employees.full_name,
+                    o.order_date,
+                    o.total_price,
+                    Статус = o.order_statuses.status_name
+                })
+                .ToList();
+            dgOrders.ItemsSource = orders;
+        }
+    }
+}
